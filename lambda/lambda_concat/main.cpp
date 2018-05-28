dadb85d1d1ad69874e10d5916d06fd9af54c9c3c
nikolaAV <alexander.nikolaenko@gmail.com>
2018-05-28 11:03:20 +0300

    update

--- lambda/lambda_concat/main.cpp
+++ lambda/lambda_concat/main.cpp
@@ -4,38 +4,16 @@
 namespace composite
 {
 
-struct start_tag {
-   template <typename param>
-   decltype(auto) operator()(param p) const {
-      return p;
-   } 
-};
-
-auto start = start_tag{};
-
 template <typename L, typename R>
-struct compose_fn {
-   L l; R r;
-   template <typename... params>
-   decltype(auto) operator()(params... ps) const {
-      return l(r(ps...));
-   } 
-};
-
-template <typename L, typename R>
-compose_fn<L,R> compose(L l, R r) {
-   return {l,r};   
+auto concat(L l, R r) {
+   return [=](auto... params) {
+      return l(r(params...));
+   };
 }
 
-template <typename... Ls, typename R>
-decltype(auto) operator | (compose_fn<Ls...> l, R r) {
-   return compose(r,l);
-}
-
-
-template <typename R>
-decltype(auto) operator | (start_tag l, R r) {
-   return compose(l,r);
+template <typename L, typename R>
+auto operator | (L l, R r) {
+   return concat(r,l);
 }
 
 } // end of namespace: composite
@@ -47,14 +25,15 @@ int main()
    /**
       mathematical statement:
        ________
-      v(x+y)*2     
+      v(x+y+z)*2     
    */
 
-   auto sum    = [](auto x, auto y) { return x+y; };   
-   auto twice  = [](auto x)         { return 2*x; };
-   auto sqrt   = [](auto x)         { return std::sqrt(x); };
+   auto sum    = [](auto x, auto y, auto z)  { return x+y+z; };   
+   auto twice  = [](auto x)                  { return 2*x; };
+   auto sqrt   = [](auto x)                  { return std::sqrt(x); };
 
-   auto formula = composite::start | sum | twice | sqrt;
-   cout << formula(4,5) << endl;
-   cout << formula(3.14,2.56) << endl;
+   using namespace composite;
+   auto formula = sum | twice | sqrt;
+   cout << formula(4,5,6) << endl;
+   cout << formula(3.14,2.56,0.1) << endl;
 }

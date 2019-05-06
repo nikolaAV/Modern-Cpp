0edd6e728adc012a8e0b1b43d711e71027644a24
nikolaAV <alexander.nikolaenko@gmail.com>
2019-05-06 13:41:27 +0300

    is_nothrow_move_constructible

---
+++ traits/is_nothrow_move_constructible/README.md
@@ -0,0 +1,16 @@
+# 'is_nothrow_move_constructible' trait by our own hands.
+```cpp
+```
+## Further informations
+* [`std::is_nothrow_move_constructible`](https://en.cppreference.com/w/cpp/types/is_move_constructible)
+* [SFINAE](https://en.cppreference.com/w/cpp/language/sfinae)
+* [C++ Templates: The Complete Guide (2nd Edition)](http://www.tmplbook.com/) by David Vandevoorde, Nicolai M. Josuttis, and Douglas Gregor. Chapter 19.7.2
+
+## Related links
+* [Detection Idiom](../../concept%20(pceudo)/detection_idiom)
+* [has_member](../../concept%20(pceudo)/has_type_member)
+
+## Compilers
+* [GCC 8.1.0](https://wandbox.org/)
+* [clang 6.0.1](https://wandbox.org/)
+* Microsoft (R) C/C++ Compiler 19.14 
---
+++ traits/is_nothrow_move_constructible/main.cpp
@@ -0,0 +1,61 @@
+#include <type_traits>
+#include <utility>
+
+template <typename T, typename = std::void_t<>>
+struct is_nothrow_move_constructible 
+   : std::false_type {
+};
+
+template <typename T>
+struct is_nothrow_move_constructible < T,
+            std::void_t<decltype(T(std::declval<T>()))> // https://github.com/nikolaAV/Modern-Cpp/tree/master/concept%20(pceudo)/detection_idiom
+         > 
+   : std::integral_constant<bool,noexcept(T(std::declval<T>()))> {
+};
+
+template <typename T>
+inline constexpr bool is_nothrow_move_constructible_v = is_nothrow_move_constructible<T>::value;
+ 
+//// Usage
+#include <string>
+
+template <typename T1, typename T2>
+struct pair {
+   T1 first;
+   T2 second;
+
+   pair(pair&& other) noexcept (is_nothrow_move_constructible_v<T1> && is_nothrow_move_constructible_v<T2>)
+      :   first(std::forward<T1>(other.first))
+         ,second(std::forward<T2>(other.second))
+   {}
+};
+
+struct can_throw {
+    can_throw(can_throw&&) {}
+};
+
+struct no_move {  // has no move ctor
+    // https://en.cppreference.com/w/cpp/types/is_move_constructible
+    // prevents implicit declaration of default move constructor
+    // however, the class is still move-constructible because its
+    // copy constructor can bind to an rvalue argument
+    no_move(const no_move&) {}
+};
+
+struct no_copy {  // non copyable
+    no_copy(const no_copy&) = delete;
+};
+
+int main()
+{
+   static_assert( is_nothrow_move_constructible_v<std::string>);
+   static_assert(!is_nothrow_move_constructible_v<can_throw>);
+   static_assert(!is_nothrow_move_constructible_v<no_move>);
+   static_assert(!is_nothrow_move_constructible_v<no_copy>);
+
+   static_assert( is_nothrow_move_constructible_v<pair<std::string,std::string>>);
+   static_assert(!is_nothrow_move_constructible_v<pair<no_move,std::string>>);
+   static_assert(!is_nothrow_move_constructible_v<pair<std::string,no_copy>>);
+   static_assert(!is_nothrow_move_constructible_v<pair<can_throw,can_throw>>);
+}
+

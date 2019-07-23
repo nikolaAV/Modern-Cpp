39a5e0e3bbf3d34cfaa5d4a991f349a7b3b8485b
nikolaAV <alexander.nikolaenko@gmail.com>
2019-07-23 12:28:10 +0300

    Update main.cpp

--- stl/optional/main.cpp
+++ stl/optional/main.cpp
@@ -8,7 +8,7 @@
 	Rather than checking for map::end you can use std::optional.
 
 	\see https://www.bfilipek.com/2018/06/optional-examples-wall.html
-	     https://github.com/nikolaAV/Modern-Cpp/tree/master/stl/optional
+	     https://github.com/nikolaAV/Modern-Cpp/tree/master/stl/optional#example-1-cleaner-interface-for-associative_containerfind
 */
 
 namespace concepts
